48a1daa1d2bda5a50cb84975c84ec25c9f28c312
Ruben <rub.per96@gmail.com>
2022-05-29 10:24:33 -0600

    implement base auth services

--- src/auth/services/security.service.spec.ts
+++ src/auth/services/security.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SecurityService } from './security.service';
+import { UserService } from '../../core/services/user.service';
+import { User } from '../../models/user.model';
 
 describe('SecurityService', () => {
   let service: SecurityService;
-
+  let userService;
   beforeEach(async () => {
+    const userServiceMethods = [
+      'findByUsername'
+    ];
+    const userServiceSpy = jasmine.createSpyObj('UserService', userServiceMethods);
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SecurityService],
+      providers: [
+        SecurityService,
+        {
+          provide: UserService,
+          useValue: userServiceSpy
+        }
+      ],
     }).compile();
 
     service = module.get<SecurityService>(SecurityService);
@@ -15,4 +27,11 @@ describe('SecurityService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should login', () => {
+    service.login('fakeuser', 'fakeuser', 'fakeuser')
+      .subscribe(console.log);
+  });
+
+
 });
--- src/auth/services/security.service.ts
+++ src/auth/services/security.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { PasswordHash } from 'auth/interfaces/_password-hash.interface';
+
 import { compare, genSalt, hash } from 'bcrypt';
+import { sign, SignOptions } from 'jsonwebtoken';
 import { combineLatest, from, map, Observable, of, switchMap } from 'rxjs';
+
+import { Role } from 'models/role.model';
+import { User } from 'models/user.model';
+
+import { Secrets } from 'constants/secrets.constant';
+import { Separators } from 'constants/separators.constant';
+
+import { UserService } from 'core/services/user.service';
+
+import { LoginResult } from 'auth/interfaces/_login-result.interface';
+import { PasswordHash } from 'auth/interfaces/_password-hash.interface';
 
 @Injectable()
 export class SecurityService {
-  constructor() {}
+  constructor(
+    private _userService: UserService,
+  ) {}
 
+  /**
+   * Generates a hash from a generated salt, and returns both the hash and the salt
+   * @param data the data for the hash
+   * @returns an observable of a PasswordHash object
+   */
   generatePasswordHash(data: string): Observable<PasswordHash> {
+    // generate salt
     return from(genSalt())
       .pipe(
         switchMap(
           (salt: string) => {
+            // generate hash
             return combineLatest<[string, string]>([
               of(salt),
               from(hash(data, salt))
@@ -20,6 +41,7 @@ export class SecurityService {
         ),
         map(
           ([salt, userHash]: [string, string]) => {
+            // return password hash object
             return { 
               salt, 
               hash: userHash 
@@ -29,7 +51,81 @@ export class SecurityService {
       );
   }
 
-  verifyPassword(data: string, hash: string): Observable<boolean> {
-    return from(compare(data, hash));
+  /**
+   * Validates if the password matches it's confirmation, and if it does, 
+   * compares the password with the stored password hash
+   *
+   * @param password password to verify
+   * @param passwordConfirmation the confirmation of the password
+   * @param passwordHash the stored hash of the password
+   * @returns an observable confirming whether the password matches the stored hash
+   */
+  verifyPassword(password: string, passwordConfirmation: string, passwordHash: string): Observable<boolean> {
+    // return false if confirmation doesnt match password
+    if (password !== passwordConfirmation) {
+      return of(false);
+    }
+    // compare password with saved hash
+    return from(compare(password, passwordHash));
+  }
+
+  /**
+   * Handles login process by verifying the password input against the stored user
+   * hash, and if the login is a success, then it generates an Access Token for the current session
+   *
+   * @param username the username of the user 
+   * @param password the password for the login
+   * @param passwordConfirmation the confirmation of the password
+   * @returns an observable with a login result
+   */
+  login(username: string, password: string, passwordConfirmation: string): Observable<LoginResult> {
+    // get user object with username
+    const user$ = from(this._userService.findByUsername(username));
+    return user$.pipe(
+      switchMap(
+        (user: User) => combineLatest([
+          of(user),
+          this.verifyPassword(password, passwordConfirmation, user.passwordHash),
+        ])
+      ),
+      map(([user, success]: [User, boolean]): LoginResult => {
+        // get roles string
+        const rolesArray = user.roles.map((role: Role, index: number) => {
+          if (index !== user.roles.length - 1) {
+            return `${role.name}${Separators.DATA}`;
+          }
+          return role.name;
+        });
+        const roles = ''.concat(...rolesArray);
+
+        // prepare response
+        let token: string;
+        let message: string;
+        if (success) {
+          // set up token payload
+          const tokenData = {
+            roles,
+            username,
+          };
+          const tokenConfig: SignOptions = {
+            algorithm: 'HS256', 
+            subject: user.uuid,
+            expiresIn: '7d',
+          }; 
+          // get token
+          token = sign(tokenData, Secrets.PASSWORD, tokenConfig);
+          message = 'success';
+        } else {
+          message = 'login error';
+        }
+        // return login response
+        return {
+          token,
+          success,
+          message,
+        }
+      })
+    )
   }
 }
+
