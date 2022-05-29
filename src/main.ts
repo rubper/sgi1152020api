8253de5cba87b0e33e5c9d3d6be34efe5e61209a
Ruben <rub.per96@gmail.com>
2022-05-29 10:45:18 -0600

    implement basic app metadata constants

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,32 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { CustomLogger } from 'loggers/custom.logger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as pjson from '../package.json';
+
+// constants forr basic app metadata
+export const APP_TITLE = 'SGI-Tin Marin API'; 
+export const APP_DESCRIPTION = pjson.description; 
+export const APP_VERSION = pjson.version; 
 
 async function bootstrap() {
+  // set up nest app
   const app = await NestFactory.create(AppModule, {
+    // set up custom logger
     logger: new CustomLogger(),
   });
+
+  // set up Open API docs config
+  const config = new DocumentBuilder()
+    .setTitle(APP_TITLE)
+    .setDescription(APP_DESCRIPTION)
+    .setVersion(APP_VERSION)
+    .addTag('museo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // listen app in port 3000
   await app.listen(3000);
 }
 bootstrap();
