88e73ff242cd96b2e114f05584bdfb48334bb48e
Ruben <rub.per96@gmail.com>
2022-05-29 21:18:18 -0600

    implements system constants

---
+++ src/constants/system.constant.ts
@@ -0,0 +1,5 @@
+import * as pjson from '../../package.json';
+// constants for basic app metadata
+export const APP_TITLE: string = 'SGI-Tin Marin API'; 
+export const APP_DESCRIPTION: string = pjson.description; 
+export const APP_VERSION: string = pjson.version;
