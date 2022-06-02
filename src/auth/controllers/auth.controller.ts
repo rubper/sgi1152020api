c0494ef9d8c710e8c619e9932099965c1900dc1b
Ruben <rub.per96@gmail.com>
2022-06-01 22:59:17 -0600

    auth no deja guardar

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Headers, Post } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
-import { IUser } from 'auth/interfaces/user.interface';
+import { ApiBearerAuth, ApiBody } from '@nestjs/swagger';
 import { LoginRequest } from 'auth/interfaces/_login-request.interface';
 import { LogoutRequest } from 'auth/interfaces/_logout-request.interface';
 import { RegisterRequest } from 'auth/interfaces/_register-request.interface';
+import { LoginBody } from 'auth/models/login.body-request';
+import { LogoutBody } from 'auth/models/logout.body-request';
+import { RegisterBody } from 'auth/models/register.body-request';
 import { SecurityService } from 'auth/services/security.service';
-import { decode, JwtPayload } from 'jsonwebtoken';
 
 @Controller('auth')
 export class AuthController {
@@ -14,17 +15,20 @@ export class AuthController {
   ) {}
   
   @Post('login')
+  @ApiBody({type: LoginBody})
   login(@Body() bodyRequest: LoginRequest) {
-    return this._securityService.login(bodyRequest.username, bodyRequest.password, bodyRequest.passwordConfirmation);
+    return this._securityService.login(bodyRequest.username, bodyRequest.password, '');
   }
 
   @Post('register')
+  @ApiBody({type: RegisterBody})
   register(@Body() bodyRequest: RegisterRequest) {
     return this._securityService.register(bodyRequest);
   }
 
-  @ApiBearerAuth()
   @Post('logout')
+  @ApiBearerAuth()
+  @ApiBody({type: LogoutBody})
   logout(@Headers() headers, @Body() bodyRequest: LogoutRequest) {
     const token = headers['authorization'];
     return this._securityService.logout(token);
