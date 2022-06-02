8bbe74704c66851a04a43118852bda937e09feee
Ruben <rub.per96@gmail.com>
2022-06-01 23:49:12 -0600

    fix prod env

--- src/scripts/set-prod-env.script.ts
+++ src/scripts/set-prod-env.script.ts
@@ -1,6 +1,16 @@
 import {rename, copyFile} from 'fs/promises';
 async function setProdEnv() {
-  await copyFile('prod.env', 'p.env');
-  await rename('p.env', '.env');
+  try {
+    await copyFile('prod.env', 'p.env');
+    console.log('copied succesfully');
+  } catch {
+    console.error('error copying');    
+  }
+  try {
+    await rename('p.env', '.env');
+    console.log('renamed succesfully');
+  } catch {
+    console.error('error renaming'); 
+  }
 }
 setProdEnv();
