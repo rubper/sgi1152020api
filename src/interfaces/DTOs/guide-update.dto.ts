586a8b0bcb19799142a2c86307040384e7b11633
Ruben <rub.per96@gmail.com>
2022-06-01 21:42:05 -0600

    implement guide dtos

---
+++ src/interfaces/DTOs/guide-update.dto.ts
@@ -0,0 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { UUID } from 'types/uuid.type';
+
+export class UpdateGuideDTO {
+  @ApiProperty({
+    description: 'El usuario asociado al guía.',
+    required: true
+  })
+  user?: UUID;
+  @ApiProperty({
+    description: 'Las horas diarias asignadas al guía.',
+    required: false
+  })
+  fixedHours?: number;
+  @ApiProperty({
+    description: 'Estampa de fecha que marca el inicio del voluntariado',
+    required: false
+  })
+  volunteershipStart?: string;
+  @ApiProperty({
+    description: 'El Estampa de fecha que marca el fin del voluntariado',
+    required: false
+  })
+  volunteershipEnd?: string;
+  @ApiProperty({
+    description: 'Total de horas que el guía ha empleado.',
+    required: false
+  })
+  hoursAggregate?: number = 0;
+  @ApiProperty({
+    description: 'Arreglo de visitas asociadas al guia.',
+    required: false
+  })
+  toursHistory?: UUID[];
+  constructor(userId: UUID, hours: number, start: string, agg?: number) {
+    this.user = userId;
+    this.fixedHours = hours;
+    this.volunteershipStart = start;
+    if (agg)
+      this.hoursAggregate = agg;
+  }
+}
