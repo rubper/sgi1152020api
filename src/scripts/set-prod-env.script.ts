f4885360fdb315c74d5b9d9fc64e8763bf3f2b38
Ruben <rub.per96@gmail.com>
2022-06-02 19:59:53 -0600

    rework set prod env script

--- src/scripts/set-prod-env.script.ts
+++ src/scripts/set-prod-env.script.ts
@@ -1,23 +1,40 @@
-import {rename, copyFile, readFile} from 'fs/promises';
+import {writeFile} from 'fs/promises';
+
+function generateEnvironmentObject(): Record<string, string> {
+  return {
+    PRODUCTION: process.env.PRODUCTION,
+    PASSWORD_SECRET: process.env.PASSWORD_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PG_HOST: process.env.PG_HOST,
+    PG_PORT: process.env.PG_PORT,
+    PG_USER: process.env.PG_USER,
+    PG_PASS: process.env.PG_PASS,
+    PG_DB: process.env.PG_DB,
+    PG_SYNCDB: process.env.PG_SYNCDB,
+    LOGGING: process.env.LOGGING,
+  }
+}
+
+function generateEnvOutputString(env: Record<string, string>) {
+  const keys = Object.keys(env);
+  let output: string = '';
+  keys.forEach(
+    (key: string) => {
+      output = output + key + '=' + env[key] + '\n';
+    }
+  )
+  return output;
+}
+
 async function setProdEnv() {
   try {
-    await copyFile('prod.env', 'p.env');
-    console.log('copied succesfully');
-  } catch {
-    console.error('error copying');    
-  }
-  try {
-    await rename('p.env', '.env');
-    console.log('renamed succesfully');
-  } catch {
-    console.error('error renaming'); 
-  }
-  try {
-    const buffer = await readFile('./.env');
-    console.log(buffer.toString());
-    
+    const envObject = generateEnvironmentObject();
+    const outputString = generateEnvOutputString(envObject);
+    console.log('generating .env file...')
+    await writeFile('.env', outputString);
+    console.log('.env file generated successfully')
   } catch (error) {
-    console.error('error reading .env'); 
+    console.error('error writing .env'); 
     console.error(error); 
     
   }
